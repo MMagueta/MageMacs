8305c13b5589c1cc130ad36d854993e02f2fcc0d
Marcos Magueta <maguetamarcos@gmail.com>
2022-10-19 23:27:40 -0300

    fix: overlay

--- flake.nix
+++ flake.nix
@@ -7,89 +7,18 @@
   };
 
   outputs = inputs@{ nixpkgs, utils, ... }:
+    {
+      overlay.default =
+        final: prev: { magemacs = import ./default.nix {pkgs = final;};};
+    } //
     utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; };
       in
       rec {
 
-        overlay.default =
-          final: prev: { magemacs = packages.default; };
-
         apps.default = utils.lib.mkApp { drv = packages.default; exePath = "/bin/emacs"; };
 
-        packages.default =
-          let
-            myEmacs = pkgs.emacs;
-            initEl = pkgs.stdenv.mkDerivation {
-              name = "MageMacsElisp";
-              src = ./.;
-              installPhase = ''
-                mkdir $out
-                mkdir $out/sources
-                cp $src/sources/* $out/sources/
-                cp *.el $out
-              '';
-            };
-            emacsWithPackages = (pkgs.emacsPackagesFor myEmacs).emacsWithPackages;
-          in
-          emacsWithPackages (epkgs: (with epkgs.melpaPackages; [
-            (pkgs.runCommand "default.el" { } ''
-              mkdir -p $out/share/emacs/site-lisp
-              cp -r ${initEl} $out/share/emacs/site-lisp
-            '')
-            yasnippet-snippets
-            yasnippet
-            use-package
-            tuareg
-            transpose-frame
-            sml-modeline
-            sml-basis
-            sly
-            request
-            rainbow-delimiters
-            projectile
-            plantuml-mode
-            pastelmac-theme
-            org-super-agenda
-            org-bullets
-            ob-sml
-            nix-mode
-            multiple-cursors
-            multi-term
-            magit
-            lsp-ui
-            lsp-treemacs
-            lsp-mode
-            lsp-metals
-            lsp-haskell
-            linum-relative
-            lfe-mode
-            hy-mode
-            helm
-            haskell-snippets
-            haskell-mode
-            fsharp-mode
-            fstar-mode
-            flymake-flycheck
-            flycheck-elsa
-            flycheck
-            eshell-syntax-highlighting
-            erlang
-            elsa
-            doom-modeline
-            diff-hl
-            dashboard
-            csharp-mode
-            company-quickhelp
-            company-erlang
-            company-coq
-            clojure-mode
-            cider
-            all-the-icons
-          ]) ++ (with epkgs.elpaPackages; [
-            yasnippet-classic-snippets
-            sml-mode
-          ]));
+        packages.default = import ./default.nix { inherit pkgs;};
 
       });
 }
