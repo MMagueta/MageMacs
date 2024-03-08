ed3a84c62a9661777bfe0c3b866e7a444e7c7076
Marcos Magueta <maguetamarcos@gmail.com>
2024-03-08 16:49:59 -0300

    move F# lsp to eglot with eldoc

--- configuration.el
+++ configuration.el
@@ -1,11 +1,9 @@
 (use-package flycheck
   :straight t
-  :defer 5
   :custom (flycheck-check-syntax-automatically '(save mode-enable)))
 
 (use-package lsp-mode
   :straight t
-  :defer 5
   :hook ((lsp-mode . lsp-lens-mode)
 	 ;; Solves F# buffer out of sync
 	 (lsp-mode . (lambda () (lsp-treemacs-sync-mode -1)))
@@ -13,14 +11,12 @@
 
 (use-package dap-mode
   :straight t
-  :defer 5
   :after lsp-mode)
 
 (require 'dap-netcore)
 
 (use-package lsp-ui
   :straight t
-  :defer 5
   :init
   (setq lsp-ui-doc-enable t)
   (setq lsp-ui-sideline-diagnostic-max-lines 7)
@@ -46,7 +42,6 @@
 	"Arguments to provide to nix-repl."
 	:type '(repeat string))
       (nix-repl)))
-  :defer 5
   :hook
   (nix-mode . lsp-deferred)
   ;; (nix-repl-mode . company-mode)
@@ -59,7 +54,6 @@
 
 (use-package swiper
   :straight t
-  :defer 5
   :bind
   ("\C-s" . swiper))
 
@@ -71,7 +65,6 @@
 
 (use-package windmove
   :straight t
-  :defer 5
   :bind
   ("C-c <right>" . windmove-right)
   ("C-c <left>" . windmove-left)
@@ -80,7 +73,6 @@
 
 (use-package perspective
   :straight t
-  :defer 5
   :bind (("C-x k" . persp-kill-buffer*))
   :init
   (persp-mode)
@@ -88,14 +80,12 @@
 
 (use-package move-text
   :straight t
-  :defer 5
   :bind
   ("M-<up>" . move-text-up)
   ("M-<down>" . move-text-down))
 
 (use-package corfu
   :straight t
-  :defer 5
   :custom ((corfu-auto t)
 	   (corfu-auto-defer 0.25)
 	   (corfu-min-width 15)
@@ -105,13 +95,30 @@
 	 (corfu-mode . corfu-popupinfo-mode)
 	 (eshell-mode . corfu-mode)))
 
+(use-package eglot
+  :straight t)
+
+(use-package eglot-fsharp
+  :straight t
+  :custom ((eglot-fsharp-server-version "0.61.1")))
+  ;; :config (eldoc-add-command #'corfu-insert))
+
+(with-eval-after-load 'eldoc
+  ;; (eldoc-add-command-completions "c-electric-")
+  (dolist (command '(corfu-insert corfu-quit))
+    (eldoc-add-command command)))
+
+(use-package eldoc-box
+  :straight t
+  :hook (eldoc-mode . eldoc-box-hover-at-point-mode))
+
 (use-package fsharp-mode
    :straight t
    :mode (("\\.fs$"  .  fsharp-mode)
 	  ("\\.fsx$" .  fsharp-mode)
 	  ("\\.fsi$" .  fsharp-mode))
-   :hook ((fsharp-mode . lsp-deferred))
-   :defer 5
+   ;; :hook ((fsharp-mode . lsp-deferred))
+   :hook ((fsharp-mode . eglot-ensure))
    :bind
    (("C-c C-,"     . 'fsharp-shift-region-left)
     ("C-c C-."     . 'fsharp-shift-region-right)
@@ -125,7 +132,6 @@
 
 (use-package magit
   :straight t
-  :defer 5
   :bind
   ("C-x g" . magit-status)
   :config
@@ -134,7 +140,6 @@
 
 (use-package helm
   :straight t
-  :defer 5
   :init
   (helm-mode t)
   (set-face-attribute 'helm-selection nil
@@ -142,11 +147,11 @@
 		      :foreground (color-darken-name (face-attribute 'default :background) 100))
   :bind
   ("M-x" . helm-M-x)
+  ("M-i" . helm-imenu)
   ("C-x b" . helm-buffers-list))
 
 (use-package multiple-cursors
   :straight t
-  :defer 5
   :bind
   ("C-c m c" . mc/edit-lines)
   ("C-<" . mc/mark-previous-like-this)
@@ -154,7 +159,6 @@
 
 (use-package projectile
   :straight t
-  :defer 5
   :init
   (projectile-mode t)
   :bind-keymap
@@ -178,12 +182,10 @@
   (setq dashboard-footer-messages '("Quod oculus non vidit, nec auris audivit - I Corinthios II,IX")))
 
 (use-package transpose-frame
-  :defer 5
   :straight t)
 
 (use-package copilot
   :straight (:host github :repo "copilot-emacs/copilot.el" :files ("dist" "*.el"))
-  :defer 5
   :bind (:map copilot-mode-map
 	      ("C-q <tab>" . copilot-accept-completion)
 	      ("C-q <right>" . copilot-next-completion)
@@ -191,37 +193,30 @@
 
 (use-package erlang
   :straight t
-  :defer 5
   :hook (erlang-mode . lsp-deferred))
 
 (use-package org-drill
-  :defer 5
   :straight t)
 
 (use-package sly
-  :defer 5
   :straight t
   :hook ((sly-mode . corfu-mode)))
 
 (use-package cider
-  :defer 5
   :straight t)
 
 (use-package clojure-mode
   :straight t
-  :defer 5
   :hook (clojure-mode . lsp-deferred))
 
 (use-package rainbow-delimiters
   :straight t
-  :defer 5
   :hook
   (lisp-mode . rainbow-delimiters-mode)
   (emacs-lisp-mode . rainbow-delimiters-mode))
 
 (use-package smartparens
   :straight t
-  :defer 5
   :hook ((lisp-mode . smartparens-mode)
 	 (emacs-lisp-mode . smartparens-mode))
   :bind (:map smartparens-mode-map
@@ -230,14 +225,13 @@
 	      ("C-M-<down>" . 'sp-down-sexp)
 	      ("C-M-<up>" . 'sp-up-sexp)
 	      ("C-k" . 'sp-kill-sexp)
-	      ("M-w" . 'sp-copy-sexp)
+	      ("M-r" . 'sp-copy-sexp)
 	      ("C-M-s" . 'sp-forward-slurp-sexp)
 	      ("C-S-s" . 'sp-backward-slurp-sexp)
 	      ("C-M-b" . 'sp-forward-barf-sexp)
 	      ("C-S-b" . 'sp-backward-barf-sexp)))
 
 (use-package protobuf-mode
-  :defer 5
   :straight t)
 
 (provide 'configuration)
